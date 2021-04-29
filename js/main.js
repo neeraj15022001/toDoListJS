fb38ad87e3dcfa5bfaa807ed32d0751eb229355e
neeraj15022001 <gneeraj32595@gmail.com>
2021-04-29 22:33:07 +0530

    added changes

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+function addElement() {
+
+}
+function removeElement() {
+
+}
+function editElement() {
+
+}
+
+// var element = document.createElement("p");
+// var text = document.createTextNode("Avishka is a good girl");
+// element.appendChild(text);
+// var elementTwo = document.createElement("p");
+// var textTwo = document.createTextNode("Avishka is a very good girl");
+// elementTwo.id = "second"
+// elementTwo.appendChild(textTwo);
+// var elementThree = document.createElement("p");
+// var textThree = document.createTextNode("Avishka is a very very good girl");
+// elementThree.appendChild(textThree);
+// const newDiv = document.getElementById("new");
+// newDiv.appendChild(element);
+// newDiv.appendChild(elementTwo)
+// newDiv.appendChild(elementThree)
+// const secondElement = document.getElementById("")
+// newDiv.insertBefore()
